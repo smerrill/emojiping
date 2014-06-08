e3239bebbbac905d3ffa273eb7fdfb8e5b2b9ff1
Steven Merrill <steven.merrill@gmail.com>
2014-06-07 21:54:23 -0400

    Initial Commit

---
+++ emojiping/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  emojiping
+//
+//  Created by Steven Merrill on 6/7/14.
+//  Copyright (c) 2014 Steven Merrill. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ emojiping/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  emojiping
+//
+//  Created by Steven Merrill on 6/7/14.
+//  Copyright (c) 2014 Steven Merrill. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
