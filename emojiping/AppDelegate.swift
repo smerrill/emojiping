46c5a6f3e58d079ed87273188aefd09b32fb7742
Steven Merrill <steven.merrill@gmail.com>
2014-06-07 22:25:09 -0400

    Add initial working NSStatusItem code.

--- emojiping/AppDelegate.swift
+++ emojiping/AppDelegate.swift
@@ -9,18 +9,29 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
-    @IBOutlet var window: NSWindow
+    @IBOutlet var statusMenu : NSMenu
 
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(NSVariableStatusItemLength))
+        self.statusItem!.title = "😃";
+        self.statusItem!.highlightMode = true;
+        self.statusItem!.menu = self.statusMenu
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    //var str = "Hello, playground"
+    //
+    //var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(NSVariableStatusItemLength))
+    //
+    //// The text that will be shown in the menu bar
+    //
+    //
+    //// The image gets a blue background when the item is selected
 }
 
