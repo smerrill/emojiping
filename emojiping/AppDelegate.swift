6aacbc747be1ef2f4c71e380e0a0535ab4e36693
Steven Merrill <steven.merrill@gmail.com>
2014-06-08 00:13:44 -0400

    It works!

--- emojiping/AppDelegate.swift
+++ emojiping/AppDelegate.swift
@@ -13,33 +13,78 @@ class AppDelegate: NSObject, NSApplicationDelegate, CDZPingerDelegate {
 
     var statusItem: NSStatusItem?
     var pinger = CDZPinger(host: "8.8.4.4")
+
+    let goodEmoji = "😃"
+    let okayEmoji = "😐"
+    let badEmoji = "😡"
+
+    let restartTimeout = 1.0
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
         self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(NSVariableStatusItemLength))
-        self.statusItem!.title = "😃";
-        self.statusItem!.highlightMode = true;
+        self.statusItem!.title = self.goodEmoji
+        self.statusItem!.highlightMode = true
         self.statusItem!.menu = self.statusMenu
 
         self.pinger.delegate = self
-        self.pinger.startPinging()
+        self.startPinging()
     }
     
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
+        self.stopPinging()
+    }
+
+    func updateItem(seconds: NSTimeInterval) {
+        // The pinger may come back before our NSStatusItem is ready. Don't let it.
+        if let item = self.statusItem {
+            // Under 300ms ping is good.
+            if seconds < 0.3 {
+                item.title = self.goodEmoji
+            }
+            else if seconds > 3.0 {
+                item.title = self.badEmoji
+            }
+            else {
+                item.title = self.okayEmoji
+            }
+        }
+    }
+
+    func startPinging() {
+        self.pinger.startPinging()
+    }
+
+    func stopPinging() {
         self.pinger.stopPinging()
     }
     
-    //var str = "Hello, playground"
-    //
-    //var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(NSVariableStatusItemLength))
-    //
-    //// The text that will be shown in the menu bar
-    //
-    //
-    //// The image gets a blue background when the item is selected
+    // CDZPingerDelegate protocol implementations
 
+    /**
+    * Called every time the pinger receives a ping back from the server.
+    *
+    * @param pinger This CDZPinger object
+    * @param seconds The average ping time, in seconds
+    */
     func pinger(pinger: CDZPinger, didUpdateWithAverageSeconds: NSTimeInterval) {
-        println(didUpdateWithAverageSeconds)
+        self.updateItem(didUpdateWithAverageSeconds)
+    }
+
+    /**
+    * Reports a ping error.
+    *
+    * Note: The pinger stops running after any error is encountered.
+    *
+    * @param pinger This CDZPinger object
+    * @param error The NSError that was encountered
+    */
+    func pinger(pinger: CDZPinger, didEncounterError: NSError) {
+        // Force a bad result.
+        self.updateItem(100.0)
+
+        // Wait 1 second, then restart the pinger.
+        var timer = NSTimer.scheduledTimerWithTimeInterval(self.restartTimeout, target: self, selector: Selector("startPinging"), userInfo: nil, repeats: false)
     }
 }
