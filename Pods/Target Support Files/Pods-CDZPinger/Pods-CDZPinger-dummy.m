def973abf465807abf4038b23e4a58dbcd026bb8
Chris Dzombak <chris@chrisdzombak.net>
2015-06-02 22:24:31 -0400

    Add Pods folder to source control
    I generally prefer this, based on reasons outlined https://www.dzombak.com/blog/2014/03/including-pods-in-source-control.html and https://guides.cocoapods.org/using/using-cocoapods.html#should-i-check-the-pods-directory-into-source-control .

---
+++ Pods/Target Support Files/Pods-CDZPinger/Pods-CDZPinger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CDZPinger : NSObject
+@end
+@implementation PodsDummy_Pods_CDZPinger
+@end
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CDZPinger
+#define COCOAPODS_POD_AVAILABLE_CDZPinger
+#define COCOAPODS_VERSION_MAJOR_CDZPinger 1
+#define COCOAPODS_VERSION_MINOR_CDZPinger 0
+#define COCOAPODS_VERSION_PATCH_CDZPinger 1
+
